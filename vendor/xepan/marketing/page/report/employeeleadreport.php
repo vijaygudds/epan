ae4999ccb0dc337f61ba653d096e74412b132226
IT <vijay.mali552@gmail.com>
2022-03-12 13:56:52 +0530

    lead report update grid list

--- vendor/xepan/marketing/page/report/employeeleadreport.php
+++ vendor/xepan/marketing/page/report/employeeleadreport.php
@@ -75,7 +75,7 @@ class page_report_employeeleadreport extends \xepan\base\Page{
 
 
 		$grid = $this->add('xepan\hr\Grid');//,null,null,['view/report/employee-lead-report-gridview']);
-		$grid->setModel($emp_model,['created_by','name','address','city','pin_code','created_at','emails_str','contacts_str','last_communication','department_id']);
+		$grid->setModel($emp_model,['created_by','generated_by','name','address','city','pin_code','created_at','emails_str','contacts_str','last_communication','department_id']);
 		$grid->add('misc/Export',['export_fields'=>['name','total_lead_created','total_lead_assign_to','total_followup','open_opportunity','qualified_opportunity','needs_analysis_opportunity','quoted_opportunity','negotiated_opportunity','win_opportunity','loss_opportunity']]);
 		$grid->addPaginator(50);
 
